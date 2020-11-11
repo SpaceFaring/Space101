bd8f39dfeed444319de66c49035fdd560bb7386d
GeorgeMalandris <georgemalandris@gmail.com>
2020-11-11 14:39:28 +0200

    enable attribute routing

--- Space101App/Space101App/App_Start/RouteConfig.cs
+++ Space101App/Space101App/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace Space101App
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
